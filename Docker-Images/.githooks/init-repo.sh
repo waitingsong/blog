99e4b13df6a0b46dece158e676bb46c27ac2ec87
waiting <1661926154@qq.com>
2022-02-09 14:07:00 +0800

    ci: add

---
+++ Docker-Images/.githooks/init-repo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# 初始化 git 仓库
+#
+# Author: waiting
+# Date: 2019.01.21
+#
+set -e
+
+git init
+git config --global i18n.commitencoding utf-8
+git config --local core.autocrlf false
+git config --local core.eol lf
+git config --local core.hooksPath ./.githooks
+git config --local core.ignorecase false
+git config --local core.precomposeUnicode true
+git config --local fetch.prune true
+git config --local pull.rebase true
+git config --local push.followTags true
+git config --local rebase.autoStash true
+git config --local remote.origin.prune true
+git config --local remote.origin.tagopt --tags
+git config --local remote.pushdefault origin
+
+
