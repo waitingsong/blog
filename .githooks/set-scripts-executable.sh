1583fa9f1574cf81f423893b979fc246b49c7f12
waiting <1661926154@qq.com>
2022-02-09 14:22:55 +0800

    fix: script

--- .githooks/set-scripts-executable.sh
+++ .githooks/set-scripts-executable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set +e
 
-find . -type f -iname "*.sh" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
-find . -type f -iname "*.mjs" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+find $cwd -type f -iname "*.sh" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+find $cwd -type f -iname "*.mjs" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
 
 set -e
 
