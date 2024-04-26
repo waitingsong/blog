9e2e1d430e20f0719fe94a2bc766dd65af1d338c
waiting <1661926154@qq.com>
2024-04-26 15:45:46 +0800

    update script

--- .githooks/set-scripts-executable.sh
+++ .githooks/set-scripts-executable.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set +e
 
-find $cwd -type f -iname "*.sh" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
 find $cwd -type f -iname "*.mjs" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+find $cwd -type f -iname "*.sh" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
+find $cwd -type f -iname "*.ts" -print0 | xargs -0II git update-index --ignore-missing --chmod=+x I
 
 set -e
 
